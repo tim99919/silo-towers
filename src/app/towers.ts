bda6559b77431ad18d2c76753444aeb1453a5633
tim99919 <tim99919@gmail.com>
2019-07-02 20:49:27 +0300

    add tower svg image component

---
+++ src/app/towers.ts
@@ -0,0 +1,16 @@
+export const towers = [
+  {
+    id: 1,
+    title: "Уровень карналита в силостной башне",
+    value: 1065,
+    minValue: 0,
+    maxValue: 1100,
+  },
+  {
+    id: 2,
+    title: "Уровень карналита в силостной башне",
+    value: 960,
+    minValue: 0,
+    maxValue: 1500,
+  }
+]
